bff9dfde0b51b4bd839aae0ed3962aa0ed2fb3ac
Ryan Castro <Ryancastro025025@gmail.com>
2024-03-22 16:43:42 -0300

    Exercicio_While_em_c#
    Exercício 1)
    WHILE: Construa um algoritmo que calcule a
    média aritmética de um conjunto de números
    pares fornecidos pelo usuário. O usuário deve
    fornecer primeiro a quantidade de números que
    serão digitados, e em seguida, os números
    considerados na média. O usuário pode digitar
    números ímpares, que devem ser ignorados.
    Por exemplo, no caso da tabela abaixo, o 5
    informa que serão digitados 5 números (2 7 8 6
    10), e para a média devem ser considerados
    apenas os números pares (2, 8, 6 e 10)
    ignorando o número 7 digitado.

---
+++ Aula_6/Ecercio_While_c#.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double somar = 0;
+            int cont_total = 0;
+            int cont_par = 0;
+            double media;
+
+            Console.WriteLine("Entre com sua quantidade de NUmeros");
+            int quantidade = int.Parse(Console.ReadLine());
+
+            while(cont_total < quantidade)
+            {
+                Console.WriteLine("Enstre com seus números");
+                int numero = int.Parse(Console.ReadLine());
+
+                if(numero % 2 == 0)
+                {
+                    somar += numero;
+                    cont_par++;
+                }
+                cont_total++;
+
+            }
+            media = somar / cont_par;
+            Console.WriteLine("Sua média é:" + media);
+            Console.ReadKey();
+        }
+    }
+}
