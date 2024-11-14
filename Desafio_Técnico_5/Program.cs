589c55091219572529a18f59f73c29e098680dc5
Ryan Castro <Ryancastro025025@gmail.com>
2024-11-14 14:34:33 -0300

    desafio tecnico 5

---
+++ Desafio_Técnico_5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {             
+            Personagem personagem1 = new Personagem("Mario", 100, 1.50);
+            Personagem personagem2 = new Personagem("Luigi", 90, 1.70);
+            personagem1.informacoes();
+            personagem1.informacoes();
+            Console.ReadKey(); 
+        }
+    }
+}
---
+++ Desafio_Técnico_5/personagem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    class Personagem
+    {
+       public string Nome { get; set; }
+       public int Vida { get; set; }
+       public double Altura { get; set; }
+
+         public Personagem(string nome, int vida, double altura)
+         {
+             Nome = nome;
+             Vida = vida;
+             Altura = altura;
+         }
+        public void informacoes()
+        {
+            Console.WriteLine($"Nome: {Nome}, Vida: {Vida}, Altura: {Altura}");
+        }
+    }
+}
+    
+
+
